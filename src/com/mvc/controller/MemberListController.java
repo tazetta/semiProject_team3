dd8e2a9b91e4e90590879b2c21fa02c0f93e7914
KHM <keg9317@naver.com>
2021-03-15 10:50:20 +0900

    관리자 상단 네비 파일명 변경, 일반회원 목록보기 작업 중

---
+++ src/com/mvc/controller/MemberListController.java
@@ -0,0 +1,43 @@
+package com.mvc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mvc.service.ManagerService;
+import com.mvc.service.MemberListService;
+
+@WebServlet({ "/memberList" })
+public class MemberListController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		dual(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		dual(req, resp);
+	}
+
+	private void dual(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		req.setCharacterEncoding("utf-8");
+		String uri = req.getRequestURI();
+		String ctx = req.getContextPath();
+		String sub = uri.substring(ctx.length());
+
+		MemberListService service = new MemberListService(req, resp);
+
+		switch (sub) {
+		case "/memberList":
+			System.out.println("일반 회원 목록 요청");
+
+		break;
+		}
+	}
+}
