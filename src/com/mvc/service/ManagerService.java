1704fc347b22725e22e1f51b73a4b9398aca1595
SDH <unfade2@gmail.com>
2021-03-15 17:10:13 +0900

    1. 관리자만 관리자 페이지에 들어올 수 있도록 처리 2. 활성화된 글만 보이도록 처리 3. 검색시 정렬 이상하게 되던 거 처리

--- src/com/mvc/service/ManagerService.java
+++ src/com/mvc/service/ManagerService.java
@@ -179,12 +179,12 @@ public class ManagerService {
 		if (dao.managerRegist(dto)) {
 			// page = "/managerList"; //페이지 닫기..?
 			msg = "관리자 등록에 성공 하였습니다.";
-			
-		}		
+
+		}
 		req.setAttribute("msg", msg);
 		dis = req.getRequestDispatcher("managerRegistClose.jsp");
 		dis.forward(req, resp);
-		 
+
 	}
 
 	private boolean isManager() {
@@ -221,7 +221,7 @@ public class ManagerService {
 			String searchType = req.getParameter("searchType");
 			String keyword = req.getParameter("keyword");
 			String isDeactivate = req.getParameter("deactivate");
-			if(isDeactivate == null) {
+			if (isDeactivate == null) {
 				isDeactivate = "FALSE";
 			}
 			System.out.println("isDeactivate : " + isDeactivate);
@@ -290,7 +290,7 @@ public class ManagerService {
 
 				tripDAO = new TripDAO();
 				TripDTO tripDTO = tripDAO.tripManageDetail(contentId);
-				
+
 				req.setAttribute("contentList", contentList);
 				req.setAttribute("largeList", largeList);
 				req.setAttribute("mediumList", mediumList);
@@ -308,54 +308,61 @@ public class ManagerService {
 	}
 
 	public void tripManageUpdate() throws ServletException, IOException {
-		String managerId = req.getParameter("managerId");
-		String contentId = req.getParameter("contentId");
-		String firstImage = req.getParameter("firstImage");
-		String latitude = req.getParameter("latitude");
-		String longitude = req.getParameter("longitude");
-		String address = req.getParameter("address");
-		String title = req.getParameter("title");
-		String contentType = req.getParameter("contentType");
-		String large = req.getParameter("large");
-		String medium = req.getParameter("medium");
-		String small = req.getParameter("small");
-		String area = req.getParameter("area");
-		String city = req.getParameter("city");
-		String overview = req.getParameter("overview");
-		String deactivate = req.getParameter("deactivate");
-		System.out.println(managerId + " / " + contentId + " / " + firstImage + " / " + latitude + " / " + longitude
-				+ " / " + address + " / " + title);
-		System.out.println(contentType + " / " + medium + " / " + small + " / " + area + " / " + city + " / "
-				+ large + " / " + overview + " / " + deactivate);
-		System.out.println(contentId + " / contentId.length" + contentId.length());
-		TripDAO tripDAO = new TripDAO();
-		TripDTO tripDTO = new TripDTO();
-		tripDTO.setManagerId(managerId);
-		tripDTO.setContentId(Integer.parseInt(contentId));
-		tripDTO.setFirstImage(firstImage);
-		tripDTO.setLatitude(latitude);
-		tripDTO.setLongitude(longitude);
-		tripDTO.setAddress(address);
-		tripDTO.setTitle(title);
-		tripDTO.setLargeIdx(large);
-		tripDTO.setContentCode(contentType);
-		tripDTO.setMediumCode(medium);
-		tripDTO.setSmallCode(small);
-		tripDTO.setAreaCode(area);
-		tripDTO.setCityCode(city);
-		tripDTO.setOverview(overview);
-		tripDTO.setDeactivate(deactivate);
-		boolean success = tripDAO.tripManageUpdate(tripDTO);
-		HashMap<String, Object> map = new HashMap<String, Object>();
 
-		map.put("success", success);
-		Gson gson = new Gson();
-		String json = gson.toJson(map);
-		resp.getWriter().print(json);
-		
+		if (isManager()) {
+			String managerId = req.getParameter("managerId");
+			String contentId = req.getParameter("contentId");
+			String firstImage = req.getParameter("firstImage");
+			String latitude = req.getParameter("latitude");
+			String longitude = req.getParameter("longitude");
+			String address = req.getParameter("address");
+			String title = req.getParameter("title");
+			String contentType = req.getParameter("contentType");
+			String large = req.getParameter("large");
+			String medium = req.getParameter("medium");
+			String small = req.getParameter("small");
+			String area = req.getParameter("area");
+			String city = req.getParameter("city");
+			String overview = req.getParameter("overview");
+			String deactivate = req.getParameter("deactivate");
+			System.out.println(managerId + " / " + contentId + " / " + firstImage + " / " + latitude + " / " + longitude
+					+ " / " + address + " / " + title);
+			System.out.println(contentType + " / " + medium + " / " + small + " / " + area + " / " + city + " / "
+					+ large + " / " + overview + " / " + deactivate);
+			System.out.println(contentId + " / contentId.length" + contentId.length());
+			TripDAO tripDAO = new TripDAO();
+			TripDTO tripDTO = new TripDTO();
+			tripDTO.setManagerId(managerId);
+			tripDTO.setContentId(Integer.parseInt(contentId));
+			tripDTO.setFirstImage(firstImage);
+			tripDTO.setLatitude(latitude);
+			tripDTO.setLongitude(longitude);
+			tripDTO.setAddress(address);
+			tripDTO.setTitle(title);
+			tripDTO.setLargeIdx(large);
+			tripDTO.setContentCode(contentType);
+			tripDTO.setMediumCode(medium);
+			tripDTO.setSmallCode(small);
+			tripDTO.setAreaCode(area);
+			tripDTO.setCityCode(city);
+			tripDTO.setOverview(overview);
+			tripDTO.setDeactivate(deactivate);
+			boolean success = tripDAO.tripManageUpdate(tripDTO);
+			HashMap<String, Object> map = new HashMap<String, Object>();
+
+			map.put("success", success);
+			Gson gson = new Gson();
+			String json = gson.toJson(map);
+			resp.getWriter().print(json);
+		} else {
+			resp.sendRedirect("index.jsp");
+		}
+
 	}
 
 	public void tripDeactivateFilter() throws ServletException, IOException {
+		if(isManager()) {
+			
 		String pageParam = req.getParameter("page");
 		int group = 1;
 		if (pageParam != null) {
@@ -372,5 +379,8 @@ public class ManagerService {
 		req.setAttribute("currPage", group);
 		RequestDispatcher dis = req.getRequestDispatcher("tripManageList.jsp");
 		dis.forward(req, resp);
+		} else {
+			resp.sendRedirect("index.jsp");
+		}
 	}
 }
