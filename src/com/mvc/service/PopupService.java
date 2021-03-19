2e11f1c5dd78a1b213573257b1675c1a1bb61dd0
성남 <rlatjdskaz@naver.com>
2021-03-19 12:18:32 +0900

    세션처리

--- src/com/mvc/service/PopupService.java
+++ src/com/mvc/service/PopupService.java
@@ -37,6 +37,9 @@ public class PopupService {
 	}
 
 	public void popupWrite() throws ServletException, IOException {
+		String isManager = (String) req.getSession().getAttribute("isManager");
+
+		if (isManager=="true") {
 			String managerid = req.getParameter("managerid");
 			String subject = req.getParameter("subject");
 			String content = req.getParameter("content");
@@ -44,7 +47,7 @@ public class PopupService {
 			
 			PopupDTO dto = new PopupDTO();
 			PopupDAO dao = new PopupDAO();
-						
+			
 			dto.setManagerid(managerid);
 			dto.setSubject(subject);
 			dto.setContent(content);
@@ -59,90 +62,129 @@ public class PopupService {
 			req.setAttribute("msg", msg);
 			dis = req.getRequestDispatcher(page);		
 			dis.forward(req, resp);					
+			
+		}else {
+			req.setAttribute("msg", "로그인 후 사용이 가능한 서비스 입니다.");
+			dis = req.getRequestDispatcher("login.jsp");
+			dis.forward(req, resp);
+		}
+		
 		}
 	
 	public void detail() throws ServletException, IOException {
-		
-		String infoidx = req.getParameter("infoidx");
-		System.out.println("상세보기 idx:"+infoidx);
-		
-		String detail = "/popupList";
-		String page = detail;
-		PopupDAO dao = new PopupDAO();
-		PopupDTO dto = dao.detail(infoidx);
-		
-		if(dto != null) {
-			dao = new PopupDAO();
-			page = "popDetail.jsp";
-			req.setAttribute("dto", dto);
+		String isManager = (String) req.getSession().getAttribute("isManager");
+
+		if (isManager=="true") {
+			String infoidx = req.getParameter("infoidx");
+			System.out.println("상세보기 idx:"+infoidx);
+			
+			String detail = "/popupList";
+			String page = detail;
+			PopupDAO dao = new PopupDAO();
+			PopupDTO dto = dao.detail(infoidx);
+			
+			if(dto != null) {
+				dao = new PopupDAO();
+				page = "popDetail.jsp";
+				req.setAttribute("dto", dto);
+			}
+			dis = req.getRequestDispatcher(page);
+			dis.forward(req, resp);			
+			
+		}else {
+			req.setAttribute("msg", "로그인 후 사용이 가능한 서비스 입니다.");
+			dis = req.getRequestDispatcher("login.jsp");
+			dis.forward(req, resp);
 		}
-		dis = req.getRequestDispatcher(page);
-		dis.forward(req, resp);			
 	}
 
 	public void updateForm() throws ServletException, IOException {
-		String infoidx = req.getParameter("infoidx");
-		System.out.println("updateForm infoidx:"+infoidx);
-		
-		PopupDAO dao = new PopupDAO();
-		PopupDTO dto = dao.detail(infoidx);
-		
-		page = "/popupList";
-		
-		if(dto!=null) {
-			page="popUpdate.jsp";
-			req.setAttribute("dto", dto);
+		String isManager = (String) req.getSession().getAttribute("isManager");
+
+		if (isManager=="true") {
+			
+			String infoidx = req.getParameter("infoidx");
+			System.out.println("updateForm infoidx:"+infoidx);
+			
+			PopupDAO dao = new PopupDAO();
+			PopupDTO dto = dao.detail(infoidx);
+			
+			page = "/popupList";
+			
+			if(dto!=null) {
+				page="popUpdate.jsp";
+				req.setAttribute("dto", dto);
+			}
+			dis = req.getRequestDispatcher(page);
+			dis.forward(req, resp);
+			
+		}else {
+			req.setAttribute("msg", "로그인 후 사용이 가능한 서비스 입니다.");
+			dis = req.getRequestDispatcher("login.jsp");
+			dis.forward(req, resp);
 		}
-		dis = req.getRequestDispatcher(page);
-		dis.forward(req, resp);
 	}
 	
-	public int update() throws ServletException, IOException {
-		
-		int success = 0;
-		String infoidx = req.getParameter("infoidx");
-		String managerid = req.getParameter("managerid");
-		String subject = req.getParameter("subject");
-		String content = req.getParameter("content");
-		String popupalert = req.getParameter("popupalert");
-		System.out.println(infoidx+"/"+managerid+"/"+subject+"/"+content+"/"+popupalert);
-		
-		PopupDAO dao = new PopupDAO();
-		PopupDTO dto = new PopupDTO ();
-		
-		dto.setInfoidx(Integer.parseInt(infoidx));
-		dto.setManagerid(managerid);
-		dto.setSubject(subject);
-		dto.setContent(content);
-		dto.setPopupalert(popupalert);
-
-		page = "/popupUpdateForm";
-		msg = "팝업 수정에 실패하였습니다.";
-		
-		if(dao.update(dto)) {
-			page="popupDetail?infoidx="+infoidx;
-			msg = "수정 성공";
+	public void update() throws ServletException, IOException {
+		String isManager = (String) req.getSession().getAttribute("isManager");
+		if (isManager=="true") {
+			String infoidx = req.getParameter("infoidx");
+			String managerid = req.getParameter("managerid");
+			String subject = req.getParameter("subject");
+			String content = req.getParameter("content");
+			String popupalert = req.getParameter("popupalert");
+			System.out.println(infoidx+"/"+managerid+"/"+subject+"/"+content+"/"+popupalert);
+			
+			PopupDAO dao = new PopupDAO();
+			PopupDTO dto = new PopupDTO ();
+			
+			dto.setInfoidx(Integer.parseInt(infoidx));
+			dto.setManagerid(managerid);
+			dto.setSubject(subject);
+			dto.setContent(content);
+			dto.setPopupalert(popupalert);
+			
+			page = "/popupUpdateForm";
+			msg = "팝업 수정에 실패하였습니다.";
+			
+			if(dao.update(dto)) {
+				page="popupDetail?infoidx="+infoidx;
+				msg = "수정 성공";
+			}
+			req.setAttribute("msg", msg);
+			dis = req.getRequestDispatcher(page);
+			dis.forward(req, resp);
+			
+		}else {
+			req.setAttribute("msg", "로그인 후 사용이 가능한 서비스 입니다.");
+			dis = req.getRequestDispatcher("login.jsp");
+			dis.forward(req, resp);
 		}
-		req.setAttribute("msg", msg);
-		dis = req.getRequestDispatcher(page);
-		dis.forward(req, resp);
-		return success;
 	}
 
 	public void popupDel() throws ServletException, IOException {
-		String infoidx = req.getParameter("infoidx");
-		System.out.println("삭제할 팝업번호 : "+infoidx);
-		
-		msg = "";
-		page ="/popupList";
-		
-		PopupDAO dao = new PopupDAO();
-		if(dao.popupDel(infoidx)) {	
-			msg="팝업을 삭제하였습니다.";
+		String isManager = (String) req.getSession().getAttribute("isManager");
+
+		if (isManager=="true") {
+			String infoidx = req.getParameter("infoidx");
+			System.out.println("삭제할 팝업번호 : "+infoidx);
+			
+			msg = "";
+			page ="/popupList";
+			
+			PopupDAO dao = new PopupDAO();
+			if(dao.popupDel(infoidx)) {	
+				msg="팝업을 삭제하였습니다.";
+			}
+			req.setAttribute("msg", msg);
+			dis = req.getRequestDispatcher(page);		
+			dis.forward(req, resp);		
+			
+		}else {
+			req.setAttribute("msg", "로그인 후 사용이 가능한 서비스 입니다.");
+			dis = req.getRequestDispatcher("login.jsp");
+			dis.forward(req, resp);
 		}
-		req.setAttribute("msg", msg);
-		dis = req.getRequestDispatcher(page);		
-		dis.forward(req, resp);		
 	}
 
 	public void popupMain() throws ServletException, IOException {
